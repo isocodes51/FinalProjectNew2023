271e32319c3c1a5ed00b07a5af339f783c892049
isocodes51 <flyingcow51@hotmail.com>
2023-05-17 01:46:15 +0300

    17.05.2023  01:48

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,7 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi";
         public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
-        internal static string MaintenanceTime = "Sistem Bakımda";
-        internal static string ProductsListed = "Ürünler Listelendi";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
     }
 }
